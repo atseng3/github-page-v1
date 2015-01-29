97d6564940cc56604de920c346ea8d4937ca2c39
Albert Tseng <atseng3@gmail.com>
2015-01-29 11:26:31 -0800

    add circle and text

--- scripts/main.js
+++ scripts/main.js
@@ -53,6 +53,7 @@ window.Profile = {
 												 .attr('stroke', 'rgb(0, 133, 88)')
 												 .attr('stroke-width', 11)
 												 .attr('fill', 'none');
+			// add endpoints
 			svg.selectAll('circle.end')
 				 .data(circle_end)
 				 .enter()
@@ -65,90 +66,28 @@ window.Profile = {
 				 .attr('stroke', "")
 				 .attr('r', 10)
 				 .attr('fill', 'rgb(0, 133, 88)');
-			svg.selectAll('circle.points')
-		   	 .data(dataset)
-		 		 .enter()
-		  	 .append('circle')
-		  	 .attr('class', 'points')
-				 .attr('cx', function(d, i) {
-				 		return (i + 0.5) * (w / dataset.length) + padding;
+			// add projects
+			var links = svg.selectAll('a')
+				 .data(dataset)
+				 .enter()
+				 .append('a')
+				 .attr('xlink:href', "google.com")
+
+  		links.append('text')
+				 .text('GOOGLE')
+				 .attr('x', function(d, i) {
+				 		return (i + 0.5) * (w / dataset.length)
 				 })
-				 .attr('cy', h / 2)
-				 .attr('r', function(d) { return d.r; })
-				 .attr('stroke', function(d) { return d.stroke; })
-				 .attr('fill', function(d) { return d.fill; });
-			// svg.select('circle').append('a').attr('href', 'google.com').html('newpage').attr('x', 10).attr('y', 60);
-				 // .data(dataset)
-				 // .enter()
-				 // .append('a')
-				 // .attr('href:xlink', function(d) { return d.text; })
-				 // .attr('x', function(d, i) {
-				 // 		return i * (w / dataset.length) + padding;
-				 // })
-				 // .attr('y', h / 2 - padding);
-			// svg.selectAll('a')
-			// 	 .data(dataset)
-			// 	 .enter()
-			//    .append('a')
-			//    .attr('xlink:href', 'google.com')
-			//    .val('s')
-			//    .attr('x', 60)
-			//    .attr('y', 21);
-			// svg.select('text')
-			//    .data(dataset)
-			// 	 .enter()
-			//    .append('text')
-			//    .attr('x', 60)
-			//    .attr('y', 21);
-			// svg.selectAll('a')
-			// 	 .data(dataset)
-			// 	 .enter()
-			// 	 .append('a')
-			// 	 .html(function(d) { return d.text; })
-			// 	 .attr('x', function(d, i) {
-			// 	 		return i * (w / dataset.length) + padding;
-			// 	 })
-			// 	 .attr('y', h / 2 - padding);
-			// svg.selectAll('text')
-			// 	 .data(dataset)
-			// 	 .enter()
-			// 	 .append('text')
-			// 	 // .text(function(d) { return d.text; })
-			// 	 .html(function(d) { return '<a href="www.google.com" target="_blank">' + d.text + '</a>'; })
-			// 	 .attr('x', function(d, i) {
-			// 	 		return i * (w / dataset.length) + padding;
-			// 	 })
-			// 	 .attr('y', h / 2 - padding)
-			// 	 .attr('font-family', 'sans-serif')
-			// 	 .attr('font-size', '15px')
-			// 	 .attr('fill', 'black')
-			// 	 .attr('text-anchor')
-
-
-
-		// <a xlink:href="https://developer.mozilla.org/en-US/docs/SVG"
-  //    target="_blank">
-  //   <rect height="30" width="120" y="0" x="0" rx="15"/>
-  //   <text fill="white" text-anchor="middle" 
-  //         y="21" x="60">SVG on MDN</text>
-  // </a>
-				 // var text = svg.selectAll('text')
-          //               .data(dataset)
-          //               .enter()
-          //               .append('text')
-          //               .text(function(d) {
-          //                   return d;
-          //               })
-          //               .attr('x', function(d, i) {
-          //                   return i * (w / dataset.length) + (w / dataset.length - barPadding) / 2;
-          //               })
-          //               .attr('y', function(d) {
-          //                   return h - (d * 4) + 14;
-          //               })
-          //               .attr('font-family', 'sans-serif')
-          //               .attr('font-size', '11px')
-          //               .attr('fill', 'white')
-          //               .attr('text-anchor', 'middle');
+				 .attr('y', h / 2 - padding);
+			
+			links.append('circle')
+					 .attr('cx', function(d, i) {
+				 			return (i + 0.5) * (w / dataset.length)
+					 })
+					 .attr('cy', h / 2)
+					 .attr('r', function(d) { return d.r; })
+					 .attr('stroke', function(d) { return d.stroke; })
+					 .attr('fill', function(d) { return d.fill; });
 			
     }
 };
