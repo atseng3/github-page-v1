f0569fccfcbf9cb9b1dc9d9370cddc70f386454e
Albert Tseng <atseng3@gmail.com>
2015-02-03 13:14:44 -0800

    project page styling

--- scripts/new.js
+++ scripts/new.js
@@ -5,23 +5,18 @@ window.Profile = {
     },
 
     setListeners: function() {
-    	$('.nav-list__items').on('click', this.handleClick);
+    	$('.projects').on('click', this.handleClick);
     },
 
     handleClick: function(event) {
     	event.preventDefault();
-    	var $currentTarget = $(event.currentTarget);
-    	if($currentTarget.hasClass('projects')) {
-    		// debugger
-    		// $('p').addClass('slideLeft');
-    		$('#projects').addClass('slideLeft');
-    		$('#desc').addClass('fadeOut');
-    		// projects = $('#projects');
-    		// if(projects)
+    	$('#projects').addClass('slideLeft');
+  		$('#desc').addClass('fadeOut');
+    	// var $currentTarget = $(event.currentTarget);
+    	// if($currentTarget.hasClass('projects')) {
     		// $('#projects').addClass('slideLeft');
-    		// $('#projects').addClass('slideRight');
-    		// $('#desc').addClass('slideLeft');
-    	}
+    		// $('#desc').addClass('fadeOut');
+    	// }
     }
 };
 
