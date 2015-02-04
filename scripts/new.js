a6a28f57fdfda2c0d213ae1319ee7f575f803e83
Albert Tseng <atseng3@gmail.com>
2015-02-03 16:09:42 -0800

    add routing for projects page

--- scripts/new.js
+++ scripts/new.js
@@ -2,14 +2,33 @@ window.Profile = {
     
     init: function () {
     	this.setListeners();
+    	this.simpleRouting();
+    },
+
+    simpleRouting: function() {
+    	var hash = window.location.hash;
+    	if(hash.match(/projects/)) {
+    		this.showProjectsPage();
+    	} else {
+    		// this.showHomePage();
+    	}
     },
 
     setListeners: function() {
-    	$('.projects').on('click', this.handleClick);
+    	$('.projects').on('click', this.showProjectsPage);
+    	$('.logo').on('click', this.showHomePage);
     },
 
-    handleClick: function(event) {
-    	event.preventDefault();
+    showHomePage: function(event) {
+    	var $projects = $('#projects');
+    	var $desc = $('#desc');
+    	$projects.removeClass('slideLeft');
+  		$projects.addClass('slideRightOut');
+  		$desc.removeClass('fadeOut');
+  		$desc.addClass('fadeIn');
+    },
+
+    showProjectsPage: function(event) {
     	var $projects = $('#projects');
     	var $desc = $('#desc');
     	if($projects.hasClass('slideLeft')) {
