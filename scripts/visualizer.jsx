66721eea7f7bcc4c834604e22d2ca7922dcd9f93
Albert Tseng <atseng3@gmail.com>
2015-02-11 15:54:25 -0800

    make visualizer page

--- scripts/visualizer.jsx
+++ scripts/visualizer.jsx
@@ -1,7 +1,13 @@
 var APP = React.createClass({
   render: function() {
-
-    return <audio src="last-ones-standing.mp3" controls preload="auto"></audio>;
+    return (
+    	<div>
+    		<input id="search-youtube" placeholder="Search Youtube"/><span className="sound"></span>
+    		<div id="visualizer-icon"></div>
+    		<progress max="100" value="80"></progress>
+    	</div>
+    )
+    // <audio src="last-ones-standing.mp3" controls preload="auto"></audio>;
   }
 });
 
