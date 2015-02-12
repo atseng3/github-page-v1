15763b8a4d7a32b5efa94729d5add8ca69e450af
Albert Tseng <atseng3@gmail.com>
2015-02-11 21:35:55 -0800

    finished autocomplete navigation and loading iframe

--- scripts/visualizer.jsx
+++ scripts/visualizer.jsx
@@ -3,7 +3,7 @@ var APP = React.createClass({
   render: function() {
     return (
     	<div>
-    		<SearchYoutube /><span className="sound"></span>
+    		<SearchYoutube />
     		<div id="visualizer-icon"></div>
     		<progress max="100" value="80"></progress>
     	</div>
@@ -16,17 +16,34 @@ var SearchYoutube = React.createClass({
 
 	getInitialState:function() {
 		return {
-			autocomplete: []
+			autocomplete: [],
+			hover: -1,
+			selected: null,
+			wantToSearch: false
 		};
 	},
 
 	updateAutocomplete: function(data) {
-		this.setState({autocomplete: data});
+		this.setState({ autocomplete: data });
+	},
+
+	handleBlur: function(event) {
+		this.updateAutocomplete([]);
+		if(event.selectedSet) {
+			$('#search-youtube').val('');
+		}
+	},
+
+	handleFocus: function(event) {
+		this.searchYoutube({
+			target: $('#search-youtube')[0]
+		})
 	},
 
 	searchYoutube: function(event) {
 		var query = $(event.target).val()
 		if(query.length < 3) {
+			this.updateAutocomplete([]);
 			return;
 		}
 		var that = this;
@@ -35,37 +52,93 @@ var SearchYoutube = React.createClass({
 	    url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=' + query + '&key=AIzaSyAYkKUPqlk7OHNTUZ7mLDBtf9bzwjQJAcY',
 	    success: function(data) {
 	    	that.updateAutocomplete(data.items);
-	    	debugger
+	    	// debugger
 	    }
 		});
 	},
 
+	navigateAutocomplete: function(event) {
+		if(event.key === 'ArrowUp') {
+			this.setState({ 
+				hover: Math.max(this.state.hover - 1, -1)
+			});
+		} else if(event.key === 'ArrowDown') {
+			this.setState({ 
+				hover: Math.min(this.state.hover + 1, 4)
+			});
+		} else if(event.key === 'Enter') {
+			if(this.state.hover > -1) {
+				this.setState({
+					hover: -1,
+					selected: this.state.autocomplete[this.state.hover],
+					wantToSearch: false
+				});
+				this.handleBlur({
+					selectedSet: true
+				});
+			}
+		} else if(event.key === 'Escape') {
+			this.handleBlur({
+				selectedSet: false
+			});
+			this.setState({
+				wantToSearch: false
+			});
+		}
+		return;
+	},
+
+	wantToSearch: function(event) {
+		this.setState({
+			wantToSearch: true
+		});
+	},
+
 	render: function() {
+		var autocomplete = '';
 		if(this.state.autocomplete.length != 0) {
-			debugger
-			var autocomplete = (
+			autocomplete = (
 				<div className="search-container__autocomplete">
-					<Autocomplete img={this.state.autocomplete[0].snippet.thumbnails.default.url} title={this.state.autocomplete[0].snippet.title}/>
-					<Autocomplete img={this.state.autocomplete[1].snippet.thumbnails.default.url} title={this.state.autocomplete[1].snippet.title}/>
-					<Autocomplete img={this.state.autocomplete[2].snippet.thumbnails.default.url} title={this.state.autocomplete[2].snippet.title}/>
-					<Autocomplete img={this.state.autocomplete[3].snippet.thumbnails.default.url} title={this.state.autocomplete[3].snippet.title}/>
-					<Autocomplete img={this.state.autocomplete[4].snippet.thumbnails.default.url} title={this.state.autocomplete[4].snippet.title}/>
+					<Autocomplete className={this.state.hover == 0 ? 'hover' : ''} img={this.state.autocomplete[0].snippet.thumbnails.default.url} title={this.state.autocomplete[0].snippet.title}/>
+					<Autocomplete className={this.state.hover == 1 ? 'hover' : ''} img={this.state.autocomplete[1].snippet.thumbnails.default.url} title={this.state.autocomplete[1].snippet.title}/>
+					<Autocomplete className={this.state.hover == 2 ? 'hover' : ''} img={this.state.autocomplete[2].snippet.thumbnails.default.url} title={this.state.autocomplete[2].snippet.title}/>
+					<Autocomplete className={this.state.hover == 3 ? 'hover' : ''} img={this.state.autocomplete[3].snippet.thumbnails.default.url} title={this.state.autocomplete[3].snippet.title}/>
+					<Autocomplete className={this.state.hover == 4 ? 'hover' : ''} img={this.state.autocomplete[4].snippet.thumbnails.default.url} title={this.state.autocomplete[4].snippet.title}/>
 				</div>
 			)
 		}
+		var iframe = '';
+		var nowPlaying = '';
+		var header = <input onChange={this.searchYoutube} id="search-youtube" placeholder="Search Youtube"/>;
+		if(this.state.selected) {
+			var src = "https://www.youtube.com/embed/" + this.state.selected.id.videoId +"?autoplay=1&autohide=0";
+			iframe = <iframe id="ytplayer" type="text/html" width="720" height="405" src={src} frameborder="0" allowfullscreen></iframe>;
+			nowPlaying = <div onClick={this.wantToSearch} id="nowPlaying">{this.state.selected.snippet.title}</div>
+			header = nowPlaying;
+		} else {
+			header = <input onChange={this.searchYoutube} id="search-youtube" placeholder="Search Youtube"/>;
+		}
+
+		if(this.state.wantToSearch) {
+			header = <input onChange={this.searchYoutube} id="search-youtube" placeholder="Search Youtube"/>;
+		}
+
 		return (
-			<div className="search-container">
-				<input onChange={this.searchYoutube} id="search-youtube" placeholder="Search Youtube"/>
+			<div className="search-container" onKeyDown={this.navigateAutocomplete} onBlur={this.handleBlur} onFocus={this.handleFocus}>
+				{header}
 				{autocomplete}
+				{iframe}
 			</div>
 		)
 	}
 });
 
+
+
 var Autocomplete = React.createClass({
 	render: function() {
 		return (
-			<div className="autocomplete__items">
+			<div className={"autocomplete__items " + this.props.className}>
 				<a href="#" className="img">
 					<img src={this.props.img} />
 				</a>
