64511cafcc4fd78b7c86cc5bc74aed5b4ad70ec0
Albert Tseng <atseng3@gmail.com>
2015-02-11 19:19:16 -0800

    finish youtube api and autocomplete

--- scripts/visualizer.jsx
+++ scripts/visualizer.jsx
@@ -1,8 +1,9 @@
 var APP = React.createClass({
+
   render: function() {
     return (
     	<div>
-    		<input id="search-youtube" placeholder="Search Youtube"/><span className="sound"></span>
+    		<SearchYoutube /><span className="sound"></span>
     		<div id="visualizer-icon"></div>
     		<progress max="100" value="80"></progress>
     	</div>
@@ -11,11 +12,72 @@ var APP = React.createClass({
   }
 });
 
-// var start = new Date().getTime();
+var SearchYoutube = React.createClass({
 
-// setInterval(function() {
-  React.render(
-    <APP />,
-    document.getElementById('container')
-  );
-// }, 50);
+	getInitialState:function() {
+		return {
+			autocomplete: []
+		};
+	},
+
+	updateAutocomplete: function(data) {
+		this.setState({autocomplete: data});
+	},
+
+	searchYoutube: function(event) {
+		var query = $(event.target).val()
+		if(query.length < 3) {
+			return;
+		}
+		var that = this;
+		$.ajax({
+	    type: 'GET',
+	    url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=' + query + '&key=AIzaSyAYkKUPqlk7OHNTUZ7mLDBtf9bzwjQJAcY',
+	    success: function(data) {
+	    	that.updateAutocomplete(data.items);
+	    	debugger
+	    }
+		});
+	},
+
+	render: function() {
+		if(this.state.autocomplete.length != 0) {
+			debugger
+			var autocomplete = (
+				<div className="search-container__autocomplete">
+					<Autocomplete img={this.state.autocomplete[0].snippet.thumbnails.default.url} title={this.state.autocomplete[0].snippet.title}/>
+					<Autocomplete img={this.state.autocomplete[1].snippet.thumbnails.default.url} title={this.state.autocomplete[1].snippet.title}/>
+					<Autocomplete img={this.state.autocomplete[2].snippet.thumbnails.default.url} title={this.state.autocomplete[2].snippet.title}/>
+					<Autocomplete img={this.state.autocomplete[3].snippet.thumbnails.default.url} title={this.state.autocomplete[3].snippet.title}/>
+					<Autocomplete img={this.state.autocomplete[4].snippet.thumbnails.default.url} title={this.state.autocomplete[4].snippet.title}/>
+				</div>
+			)
+		}
+		return (
+			<div className="search-container">
+				<input onChange={this.searchYoutube} id="search-youtube" placeholder="Search Youtube"/>
+				{autocomplete}
+			</div>
+		)
+	}
+});
+
+var Autocomplete = React.createClass({
+	render: function() {
+		return (
+			<div className="autocomplete__items">
+				<a href="#" className="img">
+					<img src={this.props.img} />
+				</a>
+				<div>
+					{this.props.title}
+				</div>
+			</div>
+		)
+	}
+});
+
+React.render(
+  <APP />,
+  document.getElementById('container')
+);
